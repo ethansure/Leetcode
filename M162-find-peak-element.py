a1fc9bb62be575ebb34b54c0fb213814567968c4
Jianxu Pan <jianxupan@foxmail.com>
2017-04-12 20:50:07 +0800

    Update m162

--- M162-find-peak-element.py
+++ M162-find-peak-element.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return nums.index(max(nums))
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + int((right -left) / 2)
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
