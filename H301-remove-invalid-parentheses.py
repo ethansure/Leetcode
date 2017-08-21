cf68aeae83262e5ceec300476e4aca4b67b72558
Jianxu Pan <jianxupancs@gmail.com>
2017-08-21 22:08:36 +0800

    update

---
+++ H301-remove-invalid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        def isvalid(s):
+            s = filter('()'.count, s)
+            while '()' in s:
+                s = s.replace('()', '')
+            return not s
+        level = {s}
+        while True:
+            valid = filter(isvalid, level)
+            if valid:
+                return valid
+            level = {s[:i] + s[i+1:] for s in level for i in range(len(s))}
