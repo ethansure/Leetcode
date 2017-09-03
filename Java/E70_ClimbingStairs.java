7da62971bbef0f24b5cd006ceef9409b6f2fd4a1
Jianxu Pan <jianxupancs@gmail.com>
2017-09-03 20:26:53 +0800

    E17

---
+++ Java/E70_ClimbingStairs.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int climbStairs(int n) {
+        int i = 1, j = 1;
+        while (n-- > 0)
+            i = (j += i) - i;
+        return i;
+    }
+}
---
+++ Java/M22_GenerateParentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        generateOneByOne("", list, n, n);
+        return list;
+    }
+    public void generateOneByOne(String sublist, List<String> list, int left, int right){
+        if(left > right){
+            return;
+        }
+        if(left > 0){
+            generateOneByOne( sublist + "(" , list, left-1, right);
+        }
+        if(right > 0){
+            generateOneByOne( sublist + ")" , list, left, right-1);
+        }
+        if(left == 0 && right == 0){
+            list.add(sublist);
+            return;
+        }
+    }
+}
