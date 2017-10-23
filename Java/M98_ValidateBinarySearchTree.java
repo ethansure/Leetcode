ce4dc541053c3467666ce2f665bd9415c1fa76e5
Jianxu Pan <jianxupancs@gmail.com>
2017-10-22 17:20:58 -0700

    add M98

--- Java/M98_ValidateBinarySearchTree.java
+++ Java/M98_ValidateBinarySearchTree.java
@@ -9,12 +9,19 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode pre = null;
+        while( !s.isEmpty() || root != null) {
+            if (root != null) {
+                s.push(root);
+                root = root.left;
+            } else {
+                root = s.pop();
+                if (pre != null && root.val <= pre.val) return false;
+                pre = root;
+                root = root.right;
+            }
+        }
+        return true;
     }
 }
