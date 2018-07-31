4baa324c8b1e6fd51ade969b4762d96825f7a488
Jianxu Pan <jianxupancs@gmail.com>
2018-07-31 08:38:59 -0700

    91 95 96

--- Java/E21_MergeTwoSortedLists.java
+++ Java/E21_MergeTwoSortedLists.java
@@ -1,3 +1,45 @@
+// New
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode cur = new ListNode(-1);
+        head = cur;
+        while(l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return head.next;
+    }
+}
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -27,3 +69,4 @@ class Solution {
         return mergeHead;
     }
 }
+
